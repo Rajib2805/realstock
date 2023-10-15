684c0969d773970b69e01c88497ca247f3096ad0
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-10-15 23:28:19 +0530

    Update app22.py

--- app22.py
+++ app22.py
@@ -293,8 +293,8 @@ def tech_indicators():
         fig = make_subplots(rows=2, cols=1, shared_xaxes=True, 
                vertical_spacing=0.03, subplot_titles=('OHLC', 'Volume'), 
                row_width=[0.2, 0.7])
-        fig.add_trace(go.Candlestick(open=df['Open'], high=df['High'], low=df['Low'], close=df['Close'], name='Stock Price'))
-        fig.add_trace(go.Bar(x=df['Date'], y=df['Volume'], name='Volume'))
+        fig.add_trace(go.Candlestick(open=df['Open'], high=df['High'], low=df['Low'], close=df['Close'], name='Stock Price'),row=1, col=1)
+        fig.add_trace(go.Bar(x=df['Date'], y=df['Volume'], name='Volume'),row=2, col=1)
         fig.update_layout(title='Stock Price and Volume', xaxis_title='Date', yaxis_title='Price')
         st.plotly_chart(fig)
 
