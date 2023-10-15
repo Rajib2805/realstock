9a07d20d7c43847e3f3667c2cb887055a7aa1ed3
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-10-15 23:01:41 +0530

    Update app22.py

--- app22.py
+++ app22.py
@@ -287,7 +287,8 @@ def tech_indicators():
         st.write('Close Price and SMA')
         st.line_chart(data_added_columns[['Close', 'SMA']])
 
-        data_added_columns = data_added_columns.reset_index(inplace=True)
+        data_added_columns.reset_index(inplace=True)
+        st.write(data_added_columns)
         df= data_added_columns
         fig = go.Figure()
         fig.add_trace(go.Candlestick(
