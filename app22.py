c1cbadae1e297feac0c0a8bdf3c780dada2eb1b0
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-10-15 22:29:26 +0530

    Update app22.py

--- app22.py
+++ app22.py
@@ -287,13 +287,7 @@ def tech_indicators():
         st.write('Close Price and SMA')
         st.line_chart(data_added_columns[['Close', 'SMA']])
 
-        stock = option # One stock prediction chart at a time for the stock chosen in the left panel dropdown
-        #stocks = ['CUBEXTUB.NS', 'AIAENG.NS',  'ASTEC.NS'] # if bultiple stock predictions are required
-        pd.options.mode.chained_assignment = None
-        tf.random.set_seed(0)
-
-        stock_short = yf.Ticker(stock)
-        df = stock_short.history(start= date.today()-timedelta(800), end= date.today(), interval= '1d')
+        df= data_added_columns
         fig = go.Figure()
         fig.add_trace(go.Candlestick(
             open=df['Open'],
